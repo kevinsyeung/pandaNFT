ab5f6f19626523207c95687d1ee1991a4ce00619
jijiBond <134910509+jijiBond@users.noreply.github.com>
2024-04-11 16:42:04 +0800

    Update MockupData.js

--- src/constants/MockupData.js
+++ src/constants/MockupData.js
@@ -3,13 +3,14 @@ import AntiqueCamera from '../assets/models/AntiqueCamera.glb';
 import BarramundiFish from '../assets/models/BarramundiFish.glb';
 import Avocado from '../assets/models/Avocado.glb';
 import CyberCar from '../assets/models/CyberCar.glb';
+import MoonMan from '../assets/1.png';
 
 export const exploreList = [
   {
-    name: "Test NFT #1",
+    name: "Moon Man",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    src: CyberCar,
+      "A man under the moon",
+    src: MoonMan,
   },
   {
     name: "Test NFT #1",
