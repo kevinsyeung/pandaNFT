d1b13e15fb9ae2e49b03c95886d65eacf677f7e7
jijiBond <134910509+jijiBond@users.noreply.github.com>
2024-04-15 16:32:54 +0800

    Add files via upload

---
+++ src/constants/MockupData.js
@@ -0,0 +1,410 @@
+//GLB MODELS
+import AntiqueCamera from '../assets/models/AntiqueCamera.glb';
+import BarramundiFish from '../assets/models/BarramundiFish.glb';
+import Avocado from '../assets/models/Avocado.glb';
+import CyberCar from '../assets/models/CyberCar.glb';
+
+//3D-model//Animal
+import Cat from '../assets/3D-models/Animal/Cat.glb';
+import Chick from '../assets/3D-models/Animal/Chick.glb';
+import Cow from '../assets/3D-models/Animal/Cow.glb';
+import Crow from '../assets/3D-models/Animal/Crow.glb';
+import Elephant from '../assets/3D-models/Animal/Elephant.glb';
+import Fox from '../assets/3D-models/Animal/Fox.glb';
+import Gazelle from '../assets/3D-models/Animal/Gazelle.glb';
+import Hen from '../assets/3D-models/Animal/Hen.glb';
+import Hornbill from '../assets/3D-models/Animal/Hornbill.glb';
+import Sealion from '../assets/3D-models/Animal/Sealion.glb';
+import Tortoise from '../assets/3D-models/Animal/Tortoise.glb';
+import Wolf from '../assets/3D-models/Animal/Wolf.glb';
+import Zebra from '../assets/3D-models/Animal/Zebra.glb';
+
+//3D-model//Character
+import Ava from '../assets/3D-models/Character/Ava.glb';
+import Bella from '../assets/3D-models/Character/Bella.glb';
+import Casey from '../assets/3D-models/Character/Casey.glb';
+import Owen from '../assets/3D-models/Character/Owen.glb';
+import Thomas from '../assets/3D-models/Character/Thomas.glb';
+import William from '../assets/3D-models/Character/William.glb';
+import zoey from '../assets/3D-models/Character/zoey.glb';
+
+//3D-model//Misc
+import plane_blue from '../assets/3D-models/Misc/plane_blue.glb';
+import red_plane_without_gear from '../assets/3D-models/Misc/red_plane_without_gear.glb';
+
+//3D-model//Nature
+import Anemone_A_01 from '../assets/3D-models/Nature/Anemone_A_01.glb';
+import Anemone_B_02 from '../assets/3D-models/Nature/Anemone_B_02.glb';
+import Conifer_01 from '../assets/3D-models/Nature/Conifer_01.glb';
+import pine_3 from '../assets/3D-models/Nature/pine_3.glb';
+import pine_4 from '../assets/3D-models/Nature/pine_4.glb';
+import Pine_06 from '../assets/3D-models/Nature/Pine_06.glb';
+import Spiker_01 from '../assets/3D-models/Nature/Spiker_01.glb';
+import Spiroots_01 from '../assets/3D-models/Nature/Spiroots_01.glb';
+import spruce_2 from '../assets/3D-models/Nature/spruce_2.glb';
+import Starfish_02 from '../assets/3D-models/Nature/Starfish_02.glb';
+import Tree_D_02 from '../assets/3D-models/Nature/Tree_D_02.glb';
+import Vine_A_01 from '../assets/3D-models/Nature/Vine_A_01.glb';
+
+//Images
+import Bulb from '../assets/Images/Abstract/Bulb.jpg';
+import Earth from '../assets/Images/Abstract/Earth.jpg';
+import HeartFace from '../assets/Images/Abstract/HeartFace.png';
+import IdontKnow from '../assets/Images/Abstract/IdontKnow.jpg';
+import MadDog from '../assets/Images/Abstract/MadDog.jpg';
+import Metal from '../assets/Images/Abstract/Metal.jpg';
+import PaintMan from '../assets/Images/Abstract/PaintMan.jpg';
+import PandaMoon from '../assets/Images/Abstract/Panda&Moon.jpg';
+import CatIcon from '../assets/Images/Animals/CatIcon.jpg';
+import CuteMonkey from '../assets/Images/Animals/CuteMonkey.png';
+import MonkeyIcon from '../assets/Images/Animals/MonkeyIcon.png';
+import SuitCat from '../assets/Images/Animals/SuitCat.jpg';
+import Alien from '../assets/Images/Human/Alien.jpg';
+import Thumb from '../assets/Images/Human/Thumb.jpg';
+import Unconsciousness from '../assets/Images/Human/Unconsciousness.png';
+import GhostIcon1 from '../assets/Images/Misc/GhostIcon1.jpg';
+import GhostIcon2 from '../assets/Images/Misc/GhostIcon2.jpg';
+import GhostIcon3 from '../assets/Images/Misc/GhostIcon3.jpg';
+import GhostIcon4 from '../assets/Images/Misc/GhostIcon4.jpg';
+import GhostIcon5 from '../assets/Images/Misc/GhostIcon5.jpg';
+import GhostIcon6 from '../assets/Images/Misc/GhostIcon6.jpg';
+import GhostIcon7 from '../assets/Images/Misc/GhostIcon7.jpg';
+import GhostIcon8 from '../assets/Images/Misc/GhostIcon8.jpg';
+import GhostIcon9 from '../assets/Images/Misc/GhostIcon9.jpg';
+import GhostIcon10 from '../assets/Images/Misc/GhostIcon10.jpg';
+import GhostIcon11 from '../assets/Images/Misc/GhostIcon11.jpg';
+import GhostIcon12 from '../assets/Images/Misc/GhostIcon12.jpg';
+import GhostIcon13 from '../assets/Images/Misc/GhostIcon13.jpg';
+import GrandMountain from '../assets/Images/Nature/GrandMountain.jpg';
+import DarkCity from '../assets/Images/Reality/DarkCity.jpg';
+
+
+export const exploreList = [
+  {
+    name: "Test NFT #1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: CyberCar,
+  },
+  {
+    name: "Test NFT #1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: AntiqueCamera,
+  },
+  {
+    name: "Test NFT #1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: BarramundiFish,
+  },
+  {
+    name: "Test NFT #1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Avocado,
+  },
+  {
+    name: "Blue Plane",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: plane_blue,
+  },
+  {
+    name: "Red planes",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: red_plane_without_gear,
+  },
+  {
+    name: "Anemone 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Anemone_A_01,
+  },
+  {
+    name: "Anemone 2",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Anemone_B_02,
+  },
+  {
+    name: "Conifer 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Conifer_01,
+  },
+  {
+    name: "Spiker 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Spiker_01,
+  },
+  {
+    name: "Spiroots",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Spiroots_01,
+  },
+  {
+    name: "Pine 3",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Pine_06,
+  },
+  {
+    name: "Starfish",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Starfish_02,
+  },
+  {
+    name: "Tree",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Tree_D_02,
+  },
+  {
+    name: "Vine",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Vine_A_01,
+  },
+  {
+    name: "Bulb",
+    description:
+      "Several bulbs flying on the sky with a human walking on the water",
+    src: Bulb,
+  },
+  {
+    name: "Earth",
+    description:
+      "",
+    src: Earth,
+  },
+  {
+    name: "HeartFace",
+    description:
+      "",
+    src: HeartFace,
+  },
+  {
+    name: "Pic",
+    description:
+      "",
+    src: IdontKnow,
+  },
+  {
+    name: "MadDog",
+    description:
+      "",
+    src: MadDog,
+  },
+  {
+    name: "Metal",
+    description:
+      "",
+    src: Metal,
+  },
+  {
+    name: "PaintMan",
+    description:
+      "",
+    src: PaintMan,
+  },
+  {
+    name: "Panda&Moon",
+    description:
+      "",
+    src: PandaMoon,
+  },
+  {
+    name: "CatIcon",
+    description:
+      "",
+    src: CatIcon,
+  },
+  {
+    name: "CuteMonkey",
+    description:
+      "",
+    src: CuteMonkey,
+  },
+  {
+    name: "MonkeyIcon",
+    description:
+      "",
+    src: MonkeyIcon,
+  },
+  {
+    name: "SuitCat",
+    description:
+      "",
+    src: SuitCat,
+  },
+  {
+    name: "Alien",
+    description:
+      "",
+    src: Alien,
+  },
+  {
+    name: "Thumb",
+    description:
+      "",
+    src: Thumb,
+  },
+  {
+    name: "Unconsciousness",
+    description:
+      "",
+    src: Unconsciousness,
+  },
+  {
+    name: "GhostIcon1",
+    description:
+      "",
+    src: GhostIcon1,
+  },
+  {
+    name: "GhostIcon2",
+    description:
+      "",
+    src: GhostIcon2,
+  },
+  {
+    name: "GhostIcon3",
+    description:
+      "",
+    src: GhostIcon3,
+  },
+  {
+    name: "GhostIcon4",
+    description:
+      "",
+    src: GhostIcon4,
+  },
+  {
+    name: "GhostIcon5",
+    description:
+      "",
+    src: GhostIcon5,
+  },
+  {
+    name: "GhostIcon6",
+    description:
+      "",
+    src: GhostIcon6,
+  },
+  {
+    name: "GhostIcon7",
+    description:
+      "",
+    src: GhostIcon7,
+  },
+  {
+    name: "GhostIcon8",
+    description:
+      "",
+    src: GhostIcon8,
+  },
+  {
+    name: "GhostIcon9",
+    description:
+      "",
+    src: GhostIcon9,
+  },
+  {
+    name: "GhostIcon10",
+    description:
+      "",
+    src: GhostIcon10,
+  },
+  {
+    name: "GhostIcon111",
+    description:
+      "",
+    src: GhostIcon11,
+  },
+  {
+    name: "GhostIcon12",
+    description:
+      "",
+    src: GhostIcon12,
+  },
+  {
+    name: "GhostIcon13",
+    description:
+      "",
+    src: GhostIcon13,
+  },
+  {
+    name: "GrandMountain",
+    description:
+      "",
+    src: GrandMountain,
+  },
+  {
+    name: "DarkCity",
+    description:
+      "",
+    src: DarkCity,
+  },
+];
+
+export const hotDropsData = [
+  {
+    name: "SuitCat",
+    description:
+      "",
+    src: SuitCat,
+  },
+  {
+    name: "DarkCity",
+    description:
+      "",
+    src: DarkCity,
+  },
+  {
+    name: "GhostIcon12",
+    description:
+      "",
+    src: GhostIcon12,
+  },
+  {
+    name: "GhostIcon4",
+    description:
+      "",
+    src: GhostIcon4,
+  },
+  {
+    name: "Unconsciousness",
+    description:
+      "",
+    src: Unconsciousness,
+  },
+  {
+    name: "Panda&Moon",
+    description:
+      "",
+    src: PandaMoon,
+  },
+  {
+    name: "CuteMonkey",
+    description:
+      "",
+    src: CuteMonkey,
+  },
+  {
+    name: "Metal",
+    description:
+      "",
+    src: Metal,
+  },
+  {
+    name: "Bulb",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: Bulb,
+  },
+];
