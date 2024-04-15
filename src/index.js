dcb4a3a80f4cc0acab53bdf7583b0faa6629db57
gorddon <79850814+gorddon@users.noreply.github.com>
2024-04-15 14:29:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,23 +7,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
 import Explore from "./pages/Explore";
+import Hunting from "./pages/Hunting";
+import IdInput from "./components/base/IdInput";
+import { GlobalProvider } from './contexts/GlobalContext';
 import { useEffect, useState } from "react";
 
 //dapp
 import { DAppProvider } from "@usedapp/core";
 import NFTDetail from "./pages/NFTDetail";
+import Login from "./pages/Login";
 
 ReactDOM.render(
-  <DAppProvider config={{}}>
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/detail" element={<NFTDetail />} />
-      </Routes>
-    </BrowserRouter>
-  </DAppProvider>,
+  <GlobalProvider>
+    <DAppProvider config={{}}>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/detail" element={<NFTDetail />} />
+          <Route path="/hunting" element={<Hunting />} />
+          {/* set id */}
+          <Route path="/id-input" element={<IdInput />} />
+        </Routes>
+      </BrowserRouter>
+    </DAppProvider>
+  </GlobalProvider>,
   document.getElementById("root")
 );
 
