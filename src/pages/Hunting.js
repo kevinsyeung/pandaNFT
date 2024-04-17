71f610dbe3cbe2f24420cfbbf70a49ff3eb7ea59
jijiBond <134910509+jijiBond@users.noreply.github.com>
2024-04-17 16:05:41 +0800

    Add files via upload

---
+++ src/pages/Hunting.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import CardList from "../components/CardList-flex";
+// import { exploreList } from "../constants/MockupData";
+import '../styles/Explore.css';
+import Header from "../components/Header";
+import Search from "../components/Search";
+import { useGlobalContext } from "../contexts/GlobalContext";
+//models
+import AntiqueCamera from '../assets/models/AntiqueCamera.glb';
+import BarramundiFish from '../assets/models/BarramundiFish.glb';
+import Avocado from '../assets/models/Avocado.glb';
+import CyberCar from '../assets/models/CyberCar.glb';
+import { type } from '@testing-library/user-event/dist/type';
+
+// context
+// import { useGlobalContext } from '../contexts/GlobalContext';
+
+// url数组
+const urls = [
+  CyberCar,
+  AntiqueCamera,
+  BarramundiFish,
+  Avocado,
+  "https://images.unsplash.com/photo-1614812513172-567d2fe96a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  "https://images.unsplash.com/photo-1541661538396-53ba2d051eed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+  "https://images.unsplash.com/photo-1573221566340-81bdde00e00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+  "https://images.unsplash.com/flagged/photo-1567934150921-7632371abb32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  "https://images.unsplash.com/photo-1608085575984-d61d8335340e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  "https://images.unsplash.com/photo-1638913970675-b5ec36292665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+]
+
+// 数据生成函数
+function generateData(ids) {
+  ids = ids.prefered_items
+  return ids.map(id => ({
+    name: `NFT #${id}`,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    src: urls[Math.floor(Math.random() * urls.length)],
+  }))
+}
+
+// 向flask请求数据
+const fetchHuntingList = async (target_id) => {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/recommend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // 将需要的参数作为请求体发送
+      body: JSON.stringify({ user_id: target_id, top_k: 20 }),
+    })
+
+    if (!response.ok) {
+      throw new Error('HTTP error! status: ${response.status}');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetching explore list failed:", error);
+    return []; // 在出错时返回空数组或其他合适的默认值
+  }
+};
+
+const Hunting = () => {
+  const { userId } = useGlobalContext();
+  const [huntingList, setHuntingList] = useState([]);
+
+  useEffect(() => {
+    const loadHuntingList = async (userId) => {
+      userId = Number(userId);
+      const data = await fetchHuntingList(userId);
+      // 生成json数据
+      const generatedList = generateData(data);
+      setHuntingList(generatedList);
+    };
+    loadHuntingList(userId);
+  }, [userId]);
+
+
+
+  return (
+    <div id="hunting">
+      <Header />
+      <Search/>
+      <div id="list-container">
+        <CardList list={huntingList} />
+      </div>
+    </div>
+  );
+};
+
+export default Hunting;
---
+++ src/pages/Login.js
@@ -0,0 +1,59 @@
+import react, {useContext} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {useEthers, useEtherBalance} from "@usedapp/core";
+import React from "react";
+import "../styles/Login.css";
+
+
+
+const Login = () => {
+    let navigate = useNavigate();
+
+    const {activateBrowserWallet, account} = useEthers();
+    const goLogin = () => {
+        debugger
+        // 获取input的username和password
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        if (username === "admin" && password === "admin") {
+            navigate("/");
+        } else {
+            // 弹框提示账户or密码错误
+            alert("username or password is wrong!")
+        }
+    };
+
+    return (
+        <div class="center">
+            <h1>Sign in/ Sign up</h1>
+            <div class="logon">
+                <div class="overlaylong">
+                    <div class="overlaylong-Signin" v-if="disfiex == 0">
+                        <h2 class="overlaylongH2">Sign in</h2>
+                        <input id ="username" type="text" placeholder="username"/>
+                        <input id ="password" type="text" placeholder="password"/>
+                        {/*<h3>Forgot your password?</h3>*/}
+                        <button class="inupbutton" id="login" onClick={goLogin}>Login</button>
+                    </div>
+
+                </div>
+                <div class="overlaytitle">
+                    <div class="overlaytitle-Signin" v-if="disfiex == 0">
+                        <h2 class="overlaytitleH2">Hello,Friend!</h2>
+                        <p class="overlaytitleP">
+                            Enter your personal details and start journey with us
+                        </p>
+                    </div>
+                    <div class="overlaytitle-Signup" v-if="disfiex == 1">
+                        <h2 class="overlaytitleH2">Welcome Back!</h2>
+                        <p class="overlaytitleP">To keep connected with us please login with your personal info</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Login;
