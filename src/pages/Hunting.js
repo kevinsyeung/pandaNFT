01fce2e0043f81a6223a4e55b78ac10628b04f87
gorddon <79850814+gorddon@users.noreply.github.com>
2024-04-17 23:53:08 +0800

    Add files via upload

--- src/pages/Hunting.js
+++ src/pages/Hunting.js
@@ -11,12 +11,36 @@ import BarramundiFish from '../assets/models/BarramundiFish.glb';
 import Avocado from '../assets/models/Avocado.glb';
 import CyberCar from '../assets/models/CyberCar.glb';
 import { type } from '@testing-library/user-event/dist/type';
+// images
+import Alien from '../assets/Images/Human/Alien.jpg';
+import Bulb from '../assets/Images/Human/Bulb.jpg';
+import HeartFace from '../assets/Images/Human/HeartFace.png';
+import IdontKnow from '../assets/Images/Human/IdontKnow.jpg';
+import MoonMan from '../assets/Images/Human/MoonMan.jpg';
+import PaintMan from '../assets/Images/Human/PaintMan.jpg';
+import Thumb from '../assets/Images/Human/Thumb.jpg';
+import Unconsciousness from '../assets/Images/Human/Unconsciousness.png';
+
+// 在原有的 urls 数组基础上，添加新的图片路径
+const additionalImages = [
+  './path/to/Images/Abstract/*',
+  './path/to/Images/Animals/*',
+  './path/to/Images/Human/*',
+  './path/to/Images/Misc/*',
+  './path/to/Images/Reality/*',
+
+
+//  './path/to/Images/Abstract/Earth.jpg',
+  // ...以此类推，对每一个文件添加一个条目
+];
+
+
 
 // context
 // import { useGlobalContext } from '../contexts/GlobalContext';
 
 // url数组
-const urls = [
+const urls_1 = [
   CyberCar,
   AntiqueCamera,
   BarramundiFish,
@@ -29,13 +53,27 @@ const urls = [
   "https://images.unsplash.com/photo-1638913970675-b5ec36292665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
 ]
 
+const humanUrl = [
+  Alien,
+  Bulb,
+  HeartFace,
+  IdontKnow,
+  MoonMan,
+  PaintMan,
+  Thumb,
+  Unconsciousness,
+]
+
+const urls = [...urls_1, ...additionalImages];
+let target_url = []
+
 // 数据生成函数
 function generateData(ids) {
   ids = ids.prefered_items
   return ids.map(id => ({
     name: `NFT #${id}`,
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    src: urls[Math.floor(Math.random() * urls.length)],
+    src: target_url[Math.floor(Math.random() * target_url.length)],
   }))
 }
 
@@ -71,6 +109,11 @@ const Hunting = () => {
     const loadHuntingList = async (userId) => {
       userId = Number(userId);
       const data = await fetchHuntingList(userId);
+      if (userId == 1) {
+        target_url = humanUrl;
+      } else if (userId == 2) {
+        target_url = urls_1;
+      }
       // 生成json数据
       const generatedList = generateData(data);
       setHuntingList(generatedList);
@@ -81,13 +124,13 @@ const Hunting = () => {
 
 
   return (
-    <div id="hunting">
-      <Header />
-      <Search/>
-      <div id="list-container">
-        <CardList list={huntingList} />
+      <div id="hunting">
+        <Header />
+        <Search/>
+        <div id="list-container">
+          <CardList list={huntingList} />
+        </div>
       </div>
-    </div>
   );
 };
 
